df08e17a76ff9ecf14efc6687aa2e69301e7fc3e
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-27 15:31:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-  let ranks = document.querySelectorAll('.ranked-list');
+  const ranks = document.querySelectorAll('.ranked-list li');
+  for (let i=0; i<ranks.length; i++){
+    ranks[i].innerHTML = (parseInt(ranks[i].innerHTML) +i).toString;
+  }
 }
