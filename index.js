e09426d92a8954e727fb2e055004b90c6a475113
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-27 15:32:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nestedTarget(){
 function increaseRankBy(n){
   const ranks = document.querySelectorAll('.ranked-list li');
   for (let i=0; i<ranks.length; i++){
-    ranks[i].innerHTML = ( parseInt(ranks[i].innerHTML) + i ).toString;
+    ranks[i].innerHTML = ( parseInt(ranks[i].innerHTML) + n ).toString;
   }
 }
